9d0f2464417bd4eaa1451ce8f29c185fc2cc2833
Roman Rader <antigluk@gmail.com>
2012-04-23 19:11:21 +0300

    Storing data to localStore; Edit items

--- src/Schedule.js
+++ src/Schedule.js
@@ -2,13 +2,40 @@ enyo.kind({
   name: "RRader.Schedule",
   kind: enyo.VFlexBox,
   components: [
+    {kind: "ApplicationEvents", onLoad: "onLoad"},
+    {kind: "Popup", name:"editPopup", components: [
+        {name:"editDayLabel"},
+        {kind: "VirtualRepeater", name: "editSubjectsList", onSetupRow: "getEditListItem",
+	          components: [
+	              {kind: "Item", layoutKind: "VFlexLayout", onclick:"editItemClick", components: [
+	                  {name: "editTitle", kind: "Divider"},
+	                  {name: "editDescription"}
+	              ]}
+	          ]},
+	    {kind: "Button", caption:"Add", onclick:"editAddClick"}
+    ]},
+    
+    {kind: "Popup", name:"editItemPopup", components: [
+    	{kind: "RowGroup", caption: "Предмет", components: [
+    		{kind: "Input", name:"editSubject", hint: "Предмет", onchange: "editSubjectChange"}
+    	]},
+    	{kind: "RowGroup", caption: "Преподователь", components: [
+    		{kind: "Input", name:"editLecturer", hint: "Преподователь", onchange: "editLecturerChange"}
+    	]},
+    	{kind: "RowGroup", caption: "Аудитория", components: [
+    		{kind: "Input", name:"editAudience", hint: "Аудитория", onchange: "editAudienceChange"}
+    	]},
+    ]},
   
   	{kind: "PageHeader", content: "Students Schedule"},
   	
   	{kind: "Control", flex:1, layoutKind: "HFlexLayout", components: [
-	  	{kind: "Scroller", flex: 3, components: [
-	          {content: "Monday", name:"dayLabel"},
-	          {kind: "VirtualRepeater", name: "mondayList", onSetupRow: "getListItem",
+	  	{kind: "Control", flex: 3, components: [
+	  		  {layoutKind: "HFlexLayout", pack:"center", align: "center", components: [
+	          	  {content: "Monday", name:"dayLabel"},
+	          	  {kind:"Button", caption: "edit", onclick:"editClicked"}
+	          ]},
+	          {kind: "VirtualRepeater", name: "subjectsList", onSetupRow: "getListItem",
 	          components: [
 	              {kind: "Item", layoutKind: "VFlexLayout", components: [
 	                  {name: "title", kind: "Divider"},
@@ -24,7 +51,7 @@ enyo.kind({
 			        {content:"2 неделя", onclick: "weekChanged"},
 	            ]},
 	            {kind: "Group", name:"weekDays", layoutKind: "VFlexLayout", components: [
-	                {kind: "Button", caption: "Понедельник", onclick: "dayChanged", toggling: true},
+	                {kind: "Button", caption: "Понедельник", onclick: "dayChanged", depressed:true, toggling: true},
 	                {kind: "Button", caption: "Вторник", onclick: "dayChanged", toggling: true},
 	                {kind: "Button", caption: "Среда", onclick: "dayChanged", toggling: true},
 	                {kind: "Button", caption: "Четверг", onclick: "dayChanged", toggling: true},
@@ -37,6 +64,29 @@ enyo.kind({
 	]}
   ],
   
+  reloadData: function() {
+  	  var selectedWeek = this.$.weekIndex.getValue();
+  	  
+  	  var container = this.$.weekDays.children.filter(function filter(x) { return (x.name=="client") } );
+      var days = container[0].children;
+      var selectedDay = 0;
+      var selectedDayLabel = "";
+      for(var i=0;i<days.length;i++) {
+          if (days[i].getDepressed()) {
+              selectedDay = i;
+              selectedDayLabel = days[i].getCaption();
+              break;
+          }
+      }
+      selectedDay += selectedWeek*7;
+      
+      this.$.dayLabel.setContent(selectedDayLabel);
+      this.currentDayLabel = selectedDayLabel;
+      this.currentDay = selectedDay;
+      this.$.subjectsList.render();
+      //localStorage
+  },
+  
   dayChanged: function(sender) {
       var container = this.$.weekDays.children.filter(function filter(x) { return (x.name=="client") } );
       var days = container[0].children;
@@ -45,9 +95,13 @@ enyo.kind({
           	  days[i].setDepressed(false);
           }
       }
-  }
-  ,
-  getListItem: function(inSender, inIndex) {
+      this.reloadData();
+  },
+  
+  weekChanged: function(sender) {
+      this.reloadData();
+  },
+  
 /*  list = [["-",
 		 {name:"Компьютерная электроника",
 		  teacher:"Виноградов Ю.Н.",
@@ -57,29 +111,104 @@ enyo.kind({
 		  teacher:"Мусина Т.В.",
 		  where:"305-18",
 		  type:"L"}]];*/
-	var list = this.list;
-	var r;
-	if (!list) {
-		return false;
-	} else {
-		r = list[0][inIndex];
-	}
-  	
-  	if (r) {
+  
+  getItemInfo: function(r, inIndex) {
   	  var title = (inIndex+1)+": ";
   	  var desc = "";
   	  if (r == "-") {
 	  	  title += "";
 	  	  desc += "Нет пары";
   	  } else {
-	      title += r.name;
-	      desc += r.where + " " + r.teacher;
+	      title += r.title;
+	      desc += r.audience + " " + r.lecturer;
       }
-      this.$.title.setCaption(title);
-	  this.$.description.setContent(desc);
+      return {title:title, desc:desc};
+  },
+  
+  getListItem: function(inSender, inIndex) {
+	if (!localStorage["day"+this.currentDay]) return false;
+	if (!JSON.parse(localStorage["day"+this.currentDay]).subjects) return false;
+	var list = JSON.parse(localStorage["day"+this.currentDay]).subjects;
+	var r = list[inIndex];
+  	
+  	if (r) {
+  	  info = this.getItemInfo(r, inIndex);
+      this.$.title.setCaption(info.title);
+	  this.$.description.setContent(info.desc);
       return true;
   	}
   	return false;
+ },
+ 
+  getEditListItem: function(inSender, inIndex) {
+	if (!localStorage["day"+this.currentDay]) return false;
+	if (!JSON.parse(localStorage["day"+this.currentDay]).subjects) return false;
+	var list = JSON.parse(localStorage["day"+this.currentDay]).subjects;
+	var r = list[inIndex];
+  	
+  	if (r) {
+  	  info = this.getItemInfo(r, inIndex);
+      this.$.editTitle.setCaption(info.title);
+	  this.$.editDescription.setContent(info.desc);
+      return true;
+  	}
+  	return false;
+ },
+ 
+ editClicked: function(sender) {
+ 	this.$.editPopup.openAtCenter();
+ 	this.$.editDayLabel.setContent(this.currentDayLabel);
+ 	this.$.editSubjectsList.render();
+ },
+ 
+ onLoad: function() {
+ 	 this.reloadData();
+ },
+ 
+ editAddClick: function() {
+     if (!localStorage["day"+this.currentDay]) {
+         localStorage["day"+this.currentDay] = "{}";
+     }
+     data = JSON.parse(localStorage["day"+this.currentDay]);
+     if (!data.subjects) {
+     	data.subjects = [];
+     }
+     data.subjects.push({title:"Empty", lecturer:"Nobody", audience:"?"});
+     localStorage["day"+this.currentDay] = JSON.stringify(data);
+     this.$.editSubjectsList.render();
+ },
+ 
+ editItemClick: function(sender, event) {
+ 	data = JSON.parse(localStorage["day"+this.currentDay]).subjects[event.rowIndex];
+ 	this.$.editSubject.setValue(data.title);
+ 	this.$.editLecturer.setValue(data.lecturer);
+ 	this.$.editAudience.setValue(data.audience);
+ 	this.currentEditingItem = event.rowIndex;
+	this.$.editItemPopup.openAtCenter();
+ },
+ 
+ editSubjectChange: function(sender) {
+ 	data = JSON.parse(localStorage["day"+this.currentDay]);
+ 	data.subjects[this.currentEditingItem].title = this.$.editSubject.getValue();
+ 	localStorage["day"+this.currentDay] = JSON.stringify(data);
+    this.$.editSubjectsList.render();
+    this.$.subjectsList.render();
+ },
+
+ editLecturerChange: function(sender) {
+ 	data = JSON.parse(localStorage["day"+this.currentDay]);
+ 	data.subjects[this.currentEditingItem].lecturer = this.$.editLecturer.getValue();
+ 	localStorage["day"+this.currentDay] = JSON.stringify(data);
+    this.$.editSubjectsList.render();
+    this.$.subjectsList.render();
+ },
+ 
+ editAudienceChange: function(sender) {
+ 	data = JSON.parse(localStorage["day"+this.currentDay]);
+ 	data.subjects[this.currentEditingItem].audience = this.$.editAudience.getValue();
+ 	localStorage["day"+this.currentDay] = JSON.stringify(data);
+    this.$.editSubjectsList.render();
+    this.$.subjectsList.render();
  }
-  
+
 });
